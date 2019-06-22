b98c3a52969289e490a450f0f9772bb13a5cc9f1
Norbert Kaźmierczak <norbini12@gmail.com>
2019-06-22 08:41:39 +0200

    NEW: Assety layoutu SB Admin 2

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,18 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/fa.min.css',
+        'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+        'css/sb-admin-2.min.css'
     ];
     public $js = [
+        'js/jquery.min.js',
+        'js/bootstrap.bundle.min.js',
+        'js/jquery.easing.min.js',
+        'js/sb-admin-2.min.js',
+        'js/Chart.min.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        
     ];
 }
